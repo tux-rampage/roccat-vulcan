07708f823fadd027284b103fed9ff7430f256626
tux-rampage <tuxrampage@gmail.com>
2021-02-07 13:19:55 +0100

    Add focal release

--- build/launchpad-ppa.sh
+++ build/launchpad-ppa.sh
@@ -4,8 +4,10 @@ cd $(dirname $(realpath $0))/../src
 
 DATE=$(date -R)
 VERSION="1.1.3-1"
+[[ -z $PPA_USER ]] && PPA_USER='Tom Kistner <tom@kistner.nu>'
+[[ -z $PPA_TARGET ]] && PPA_TARGET='ppa:duncanthrax/roccat-vulcan'
 
-for RELEASE in eoan disco cosmic bionic
+for RELEASE in focal eoan disco cosmic bionic
 do
 
 cat >debian/changelog <<END
@@ -13,11 +15,11 @@ roccat-vulcan ($VERSION~$RELEASE) $RELEASE; urgency=medium
 
   * Update to $VERSION
 
- -- Tom Kistner <tom@kistner.nu>  $DATE
+ -- $PPA_USER  $DATE
 END
 
 debuild -S | tee debuild.log 2>&1
-dput ppa:duncanthrax/roccat-vulcan "$(perl -ne 'print $1 if /dpkg-genchanges --build=source >(.*)/' debuild.log)"
+dput $PPA_TARGET "$(perl -ne 'print $1 if /dpkg-genchanges --build=source >(.*)/' debuild.log)"
 rm -f debuild.log
 
 done
